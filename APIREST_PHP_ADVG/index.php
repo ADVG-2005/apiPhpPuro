c1194c21f3cdf81c0b3d13c6cca22b1577d7a981
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-01 18:51:39 -0500

    Excluyendo endpoint de usuarios con el metodo POST para realizar un nuevo registro

--- APIREST_PHP_ADVG/index.php
+++ APIREST_PHP_ADVG/index.php
@@ -14,38 +14,48 @@ use Firebase\JWT\Key;
 $secretKey = "miclavesecreta";
 
 $request = explode('/', trim($_SERVER["REQUEST_URI"]));
-$controller = ucfirst(strtolower($request[2])) . "Controller" ?? ''; //definir la posicion que llevara el endpoint en la uri 
+$controller = ucfirst(strtolower($request[1])) . "Controller" ?? ''; //definir la posicion que llevara el endpoint en la uri 
 $method = $_SERVER['REQUEST_METHOD'];
-$id = $request[3] ?? null; // definir la posicion que llevara el id en la uri 
+$id = $request[2] ?? null; // definir la posicion que llevara el id en la uri 
 
-if ($request[2] == "authController") {
+if ($request[1] == "authController") {
     require 'Controllers/authController.php';
+    exit;
+}
+if ($request[1] == "usuariosController"){
+    require 'Controller/usuariosController.php';
     exit;
 }
 
 $dir = __DIR__ . "/Controllers/" . $controller . ".php";
 
 if (file_exists($dir)) {
-    // Excluir authController de la protección con JWT
-    if ($controller !== "AuthController") {
+
+    // Excluir authController  de la protección con JWT
+    if (
+        !($controller === "AuthController") &&
+    // Excluir metodo POST dentro de usuariosController para realizar un registro y poder realizar el inicio de sesion 
+        !($controller === "UsuariosController" && $method === "POST")
+    ) {
         $headers = getallheaders();
         if (!isset($headers['Authorization'])) {
             http_response_code(401);
             echo json_encode(["error" => "No token provided"]);
             exit;
         }
-
+    
         $token = str_replace('Bearer ', '', $headers['Authorization']);
-        
+    
         try {
             $decoded = JWT::decode($token, new Key($secretKey, 'HS256'));
-            $_SESSION['user'] = $decoded; // Guardamos el usuario en sesión si es necesario
+            $_SESSION['user'] = $decoded;
         } catch (Exception $e) {
             http_response_code(401);
             echo json_encode(["error" => "Invalid token"]);
             exit;
         }
     }
+    
 
     require_once $dir;
     $claseController = new $controller();
